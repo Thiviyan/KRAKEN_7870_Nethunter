e7b52dc81fd7af04d8e0e916664027e7fd84ea86
Steven Barrett <damentz@liquorix.net>
2016-03-15 18:17:03 +0100

    ZEN: Implement zen-tune v4.5
    Zen-tune used to use a low background and dirty ratio so we would begin
    writeback as soon as possible. However, even under 'regular' heavy load,
    the system would stall regardless of how low the writeback numbers were.
    
    The Linux kernel, when under heavy IO load, tends to wait for all requests
    to the scheduler to complete before loading the next group. This causes
    lengthy stalls that make the system unusable interactively. As you
    increase BLKDEV_MAX_RQ, the system stalls increase. Reducing this number
    to as low as 16 doesn't impact throughput _that_ much, but mostly fixes
    all stalls, such as the UI and mouse freezing.
    
    Use a reduced NCQ queue depth to prevent the storage device from causing
    latency spikes during heavy IO. This has the side effect of reducing
    throughput only a bit, but significantly reducing the chance that entire
    system stalls occur.
    
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -71,7 +71,9 @@ static long ratelimit_pages = 32;
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-#ifdef CONFIG_LARGE_DIRTY_BUFFER
+#ifdef CONFIG_ZEN_INTERACTIVE
+int dirty_background_ratio = 20;
+#elif CONFIG_LARGE_DIRTY_BUFFER
 int dirty_background_ratio = 5;
 #else
 int dirty_background_ratio;
@@ -96,10 +98,12 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-#ifdef CONFIG_LARGE_DIRTY_BUFFER
-int vm_dirty_ratio = 20;
+#ifdef CONFIG_ZEN_INTERACTIVE
+int vm_dirty_ratio = 50;
+#elif CONFIG_LARGE_DIRTY_BUFFER
+int vm_dirty_ratio = 25;
 #else
-int vm_dirty_ratio;
+int vm_dirty_ratio = 20;
 #endif
 
 /*
