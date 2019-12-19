f5deba8ecca7bf99e2714d192d142813ad405282
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-12-19 21:22:20 +0800

    blkdev: disable IO stat

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -520,7 +520,7 @@ struct request_queue {
 #define QUEUE_FLAG_NO_SG_MERGE 21	/* don't attempt to merge SG segments*/
 #define QUEUE_FLAG_SG_GAPS     22	/* queue doesn't support SG gaps */
 
-#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
+#define QUEUE_FLAG_DEFAULT	((0 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
 				 (1 << QUEUE_FLAG_ADD_RANDOM))
