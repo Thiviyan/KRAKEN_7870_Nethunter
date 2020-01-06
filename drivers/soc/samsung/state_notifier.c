2642f1011349f64b5cbb258538e8cc54a2c22d11
spart4nicus <paradelovhristiyan@gmail.com>
2020-01-06 13:33:20 +0100

    state_notifier: Reduce defer on suspend call to 1 second
    Signed-off-by: spart4nicus <paradelovhristiyan@gmail.com>

--- drivers/soc/samsung/state_notifier.c
+++ drivers/soc/samsung/state_notifier.c
@@ -16,7 +16,7 @@
 #include <linux/mutex.h>
 #include "../../../kernel/power/power.h"
 
-#define DEFAULT_SUSPEND_DEFER_TIME 	10
+#define DEFAULT_SUSPEND_DEFER_TIME 	1
 #define STATE_NOTIFIER			"state_notifier"
 
 /*
