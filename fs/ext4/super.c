be1e26b1ba984e35d0477003ca9de99f23234dc3
spart4nicus <paradelovhristiyan@gmail.com>
2020-01-07 21:31:47 +0100

    fs: ext4: super: merge some changes
    Signed-off-by: spart4nicus <paradelovhristiyan@gmail.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -732,6 +732,10 @@ __acquires(bitlock)
 	}
 
 	ext4_unlock_group(sb, grp);
+	ext4_handle_error(sb, page_buf);
+	if (page_buf)
+		free_page((unsigned long)page_buf);
+	ext4_commit_super(sb, 1);
 	/*
 	 * We only get here in the ERRORS_RO case; relocking the group
 	 * may be dangerous, but nothing bad will happen since the
@@ -4586,10 +4590,8 @@ cantfind_ext4:
 	/* for debugging, sangwoo2.lee */
 	/* If you wanna use the flag 'MS_SILENT', call */
 	/* 'print_bh' function within below 'if'. */
-	if (!silent) {
-		printk(KERN_ERR "printing data of superblock-bh\n");
-		print_bh(sb, bh, 0, EXT4_BLOCK_SIZE(sb));
-	}
+	printk(KERN_ERR "printing data of superblock-bh\n");
+	print_bh(sb, bh, 0, EXT4_BLOCK_SIZE(sb));
 	/* for debugging */
 
 	if (!silent)
@@ -6152,3 +6154,4 @@ MODULE_DESCRIPTION("Fourth Extended Filesystem");
 MODULE_LICENSE("GPL");
 module_init(ext4_init_fs)
 module_exit(ext4_exit_fs)
+
