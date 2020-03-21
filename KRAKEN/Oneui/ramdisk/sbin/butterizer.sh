68840acfd915039bdf19f32d97d42d82a4eca649
sp4rt4c <paradelovhristiyan@gmail.com>
2020-03-21 12:32:32 +0100

    ramdisk: butterizer: nuke some functions
    Signed-off-by: sp4rt4c <paradelovhristiyan@gmail.com>

--- KRAKEN/Oneui/ramdisk/sbin/butterizer.sh
+++ KRAKEN/Oneui/ramdisk/sbin/butterizer.sh
@@ -12,21 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Butterizer 1.4
+# Butterizer 1.6
 # Originally Coded by SPARTANICUS, iGlitch @XDAdevelopers
 
 log_print "Butterizer process started"
-
 # mount system as rw;
 mount -o remount,rw /system
-
-# Disable init.exynos7870.rc to prevent VENDOR interference;
-if [ -e /vendor/etc/init.exynos7870.rc ]; then
-  mount -o remount,rw /system;
-  mv /vendor/etc/init.exynos7870.rc /vendor/etc/init.exynos7870.rc.bak;
-  mount -o remount,ro /system;
-fi;
-
 # Frequency tweaks;
 echo "1" > /sys/devices/system/cpu/cpu0/online
 echo "interactives9" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -71,7 +62,6 @@ echo "80 343000:95 449000:90 546000:85 676000:85 757000:80 839000:80 902000:75 1
 #echo "20000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
 #echo "80000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/boostpulse_duration
 #echo "80 343000:95 449000:90 546000:85 676000:85 757000:80 839000:80 902000:75 1014000:70 1144000:65 1248000:65 1352000:65 1482000:60 1586000:45 1690000:20" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
-
 # Frequency tweaks;
 echo "1" > /sys/devices/system/cpu/cpu4/online
 echo "interactives9" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
@@ -116,18 +106,15 @@ echo "80 343000:95 449000:90 546000:85 676000:85 757000:80 839000:80 902000:75 1
 #echo "20000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
 #echo "80000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/boostpulse_duration
 #echo "74 757000:80 839000:77 902000:75 1014000:70 1144000:65 1248000:62 1352000:65 1482000:60 1586000:45 1690000:20" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
-
 # Performance oriented HMP profile;
 echo "410" > /sys/kernel/hmp/up_threshold
 echo "130" > /sys/kernel/hmp/down_threshold
-
 # A custom CPUSet profile;
 echo "0-2,4-7" > /dev/cpuset/foreground/cpus
 echo "4-7" > /dev/cpuset/foreground/boost/cpus
 echo "0-7" > /dev/cpuset/top-app/cpus
 echo "0-2" > /dev/cpuset/system-background/cpus
 echo "0-3" > /dev/cpuset/restricted/cpus
-
 # Mali-T830 MP1 optimization into delivering better overall graphical rendering performance combined with great UI experience and battery savings;
 echo "interactive" > /sys/devices/11400000.mali/dvfs_governor
 echo "driver" > /sys/devices/11400000.mali/core_availability_policy
@@ -135,50 +122,23 @@ echo "coarse_demand" > /sys/devices/11400000.mali/power_policy
 echo "1246" > /sys/devices/11400000.mali/max_clock
 echo "343" > /sys/devices/11400000.mali/min_clock 343
 echo "60" > /sys/devices/platform/gpusysfs/fps
-
 # Enable Dynamic Fsync;
 echo "1" > /sys/kernel/dyn_fsync/Dyn_fsync_active
-
-# Virtual Memory tweaks & enhancements for a massively improved balance between performance and battery life;
-echo "3" > /proc/sys/vm/dirty_background_ratio
-echo "500" > /proc/sys/vm/dirty_expire_centisecs
-echo "30" > /proc/sys/vm/dirty_ratio
-echo "3000" > /proc/sys/vm/dirty_writeback_centisecs
-echo "1200" > /proc/sys/vm/stat_interval
-echo "20" > /proc/sys/vm/swappiness
-echo "60" > /proc/sys/vm/vfs_cache_pressure
-
-# Disable exception-trace and reduce some overhead that is caused by a certain amount and percent of kernel logging, in case your kernel of choice have it enabled;
-echo "0" > /proc/sys/debug/exception-trace
-
 # FileSystem (FS) optimized tweaks & enhancements for a improved userspace experience;
 echo "0" > /proc/sys/fs/dir-notify-enable
 echo "20" > /proc/sys/fs/lease-break-time
-
 # A couple of minor kernel entropy tweaks & enhancements for a slight UI responsivness boost;
 echo "128" > /proc/sys/kernel/random/read_wakeup_threshold
 echo "96" > /proc/sys/kernel/random/urandom_min_reseed_secs
 echo "2560" > /proc/sys/kernel/random/write_wakeup_threshold
-
-# Kernel based tweaks that reduces the total amount of wasted CPU cycles and gives back a huge amount of needed performance as well as battery life savings to both the whole system and the user experience itself;
-echo "0" > /proc/sys/kernel/compat-log
-echo "0" > /proc/sys/kernel/panic
-echo "0" > /proc/sys/kernel/panic_on_oops
-echo "0" > /proc/sys/kernel/perf_cpu_time_max_percent
-echo "15000000" > /proc/sys/kernel/sched_latency_ns
-echo "2000000" > /proc/sys/kernel/sched_min_granularity_ns
-echo "10000000" > /proc/sys/kernel/sched_wakeup_granularity_ns
-
 # Enable CFQ group idle mode for improved scheduling effectivness by merging the IO queues in a "unified group" instead of treating them as individual IO based queues;
 for i in /sys/block/*/queue/iosched; do
   echo "1" > $i/group_idle;
 done;
-
 # Disable CFQ low latency mode for overall increased IO based scheduling throughput and for better overall needed responsivness & performance from the system;
 for i in /sys/block/*/queue/iosched; do
   echo "0" > $i/low_latency;
 done;
-
 # Wide block based tuning for reduced lag and less possible amount of general IO scheduling based overhead (Thanks to pkgnex @ XDA for the more than pretty much simplified version of this tweak. You really rock, dude!);
 for i in /sys/block/*/queue; do
   echo "0" > $i/add_random;
@@ -189,33 +149,27 @@ for i in /sys/block/*/queue; do
   echo "0" > $i/rotational;
   echo "1" > $i/rq_affinity;
 done;
-
 # Tweak the kernel task scheduler for improved overall system performance and user interface responsivness during all kind of possible workload based scenarios;
 echo "NO_GENTLE_FAIR_SLEEPERS" > /sys/kernel/debug/sched_features
 echo "NO_TTWU_QUEUE" > /sys/kernel/debug/sched_features
 echo "NO_RT_RUNTIME_SHARE" > /sys/kernel/debug/sched_features
-
 # Tweak and decrease tx_queue_len default stock value(s) for less amount of generated bufferbloat and for gaining slightly faster network speed and performance;
 for i in $(find /sys/class/net -type l); do
   echo "128" > $i/tx_queue_len;
 done;
-
 # A miscellaneous pm_async tweak that increases the amount of time (in milliseconds) before user processes & kernel threads are being frozen & "put to sleep";
 echo "25000" > /sys/power/pm_freeze_timeout
-
 # Clean previous logs;
 rm -f /storage/emulated/0/*.log;
-
 # Script log file location;
 LOG_FILE=/storage/emulated/0
 echo $(date) > /storage/emulated/0/butterizer.log
 if [ $? -eq 0 ]
 then
-  echo "Project Butterizer was executed successfully! You're currently running on Butterizer 1.5!" >> /storage/emulated/0/butterizer.log
+  echo "Project Butterizer was executed successfully! You're currently running on Butterizer 1.6!" >> /storage/emulated/0/butterizer.log
   exit 0
 else
   echo "Project Butterizer wasn't executed successfully!" >> /storage/emulated/0/butterizer.log
   exit 1
 fi
-
 # The End;
