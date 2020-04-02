abecca5671890bb395e7c71edab463820f179fe0
sp4rt4c <paradelovhristiyan@gmail.com>
2020-04-02 14:37:31 +0200

    drivers: soc: set max freq at 1.69 GHz
    Signed-off-by: sp4rt4c <paradelovhristiyan@gmail.com>

--- drivers/thermal/samsung/exynos_tmu.c
+++ drivers/thermal/samsung/exynos_tmu.c
@@ -1142,7 +1142,7 @@ static int exynos_tmu_ect_set_information(struct platform_device *pdev)
 		else
 			pdata->trigger_type[i] = (i == function->num_of_range - 1 ? HW_TRIP : THROTTLE_ACTIVE);
 
-		if (function->range_list[i].max_frequency == 1586000)
+		if (function->range_list[i].max_frequency == 1690000)
 			function->range_list[i].max_frequency = 1794000;
 
 		pdata->freq_tab[i].temp_level = function->range_list[i].lower_bound_temperature;
